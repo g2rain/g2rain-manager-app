d270ea9953e2271c1550151846f6df391e4d824d
SUNKING <sunhj000@163.com>
2026-07-17 09:04:48 +0800

    同步数据

--- src/views/idp_enterprise_organ/index.vue
+++ src/views/idp_enterprise_organ/index.vue
@@ -186,7 +186,7 @@
           <span>
             <el-button
               type="warning"
-              v-permission="'idp_enterprise_organ:edit'"
+              v-permission="'idp_enterprise_organ:sync'"
               :loading="syncLoading"
               :disabled="!canSyncCurrentRow"
               @click="handleSync"
